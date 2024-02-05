986cbc7414b624214704183c71fe1e03bc92480b
김창훈 <expelia81@gmail.com>
2024-02-05 20:10:55 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (37.15%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -17,23 +17,11 @@ class Solution {
     Stack<Integer> a = new Stack<>();
     Stack<Integer> b = new Stack<>();
     public boolean isSymmetric(TreeNode root) {
-        
-//         TreeNode left = root.left;
-//         TreeNode right = root.right;
-        
-//         findLeftTree(left, a);
-        
-//         findRightTree(right, b);
-        
-//         return isSame();
-        
         return isSame(root.left, root.right);
         
     }
     
     
-    
-    // 깊이가 다른 경우엔 어떡하지
     boolean isSame(TreeNode left, TreeNode right) {
         if(left == null && right ==null) {
             return true;
@@ -44,27 +32,4 @@ class Solution {
         return false;
     }
     
-    void findLeftTree(TreeNode root, Stack<Integer> stack) {
-        
-        
-        if(root.left != null) {
-            findLeftTree(root.left, stack);
-        }
-        stack.push(root.val);
-        if(root.right != null) {
-            findLeftTree(root.right, stack);
-        }
-    }
-    void findRightTree(TreeNode root, Stack<Integer> stack) {
-        
-        
-        if(root.right != null) {
-            findRightTree(root.right, stack);
-        }
-        
-        stack.push(root.val);
-        if(root.left != null) {
-            findRightTree(root.left, stack);
-        }
-    }
 }
