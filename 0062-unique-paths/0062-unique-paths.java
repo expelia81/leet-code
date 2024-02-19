f39ec3c9e9c30967654afb193eaa861a4c877f33
김창훈 <expelia81@gmail.com>
2024-02-19 20:56:20 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (22.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        
-    int[][] arr = new int[m][n];
+        int[][] arr = new int[m][n];
         for(int i =0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(j==0 || i==0) {
@@ -12,17 +11,5 @@ class Solution {
             }
         }
         return arr[m-1][n-1];
-    }
-    
-    int func(int x, int y) {
-        System.out.println(x + " -- " + y);
-        
-        if(x == 0 || y == 0) {
-            return 1;
-        }
-        
-        int xVal = x==0 ? 0 : func(x-1,y);
-        int yVal = y==0 ? 0 : func(x,y-1);
-        return xVal + yVal;
     }
 }
