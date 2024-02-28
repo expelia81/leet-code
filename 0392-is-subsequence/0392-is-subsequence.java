4e8b3951b3c5f434033c94575f4b84c3f54d8e3c
김창훈 <expelia81@gmail.com>
2024-02-28 20:14:01 +0900

    Time: 60 ms (6.31%), Space: 45.1 MB (5.22%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        // two pointer
+        String[] target = s.split("");
+        String[] all = t.split("");
+        
+        int targetIndex = 0;
+        int allIndex = 0;
+        
+        if(s.length()==0) {
+            return true;
+        }
+        if(t.length()==0) {
+            return false;
+        }
+        
+        
+        
+        while(targetIndex != target.length && allIndex != all.length) {
+            
+            System.out.println("targetIndex : " + targetIndex + " allIndex : " +allIndex);
+            
+            if(all[allIndex].equals(target[targetIndex])){
+                targetIndex++;
+                allIndex++;
+            } else {
+                allIndex++;
+            }
+        }
+        
+        
+        
+        if(targetIndex==target.length) {
+            return true;
+        }
+        return false;
+    }
+}
