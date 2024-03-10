6c5c9272827a9928c8093b137bca5f655b452e6d
김창훈 <expelia81@gmail.com>
2024-03-11 00:19:53 +0900

    Time: 15 ms (11.28%), Space: 46.5 MB (45.97%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,18 +1,10 @@
 class Solution {
     public int arrayPairSum(int[] arr) {
-        // arr = 
-           int[] test = Arrays.stream(arr)
-                .sorted()
-            // .forEach(s->System.out.println(s+""));
-            .toArray();
         
-        // for(int i:test) {
-        //     System.out.println("result : " + i);
-        // }
+        Arrays.sort(arr);
         int result = 0;
         for(int i=0;i<arr.length/2;i++) {
-            int temp = Math.min(test[i*2],test[i*2+1]);
-            // System.out.println("temp : " + temp);
+            int temp = Math.min(arr[i*2],arr[i*2+1]);
             result += temp;
         }
         
