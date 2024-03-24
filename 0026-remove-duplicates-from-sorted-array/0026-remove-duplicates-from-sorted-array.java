4558004fa7aab10297f256c6719b5cd67d3d1501
김창훈 <expelia81@gmail.com>
2024-03-24 19:46:06 +0900

    Time: 7 ms (5.36%), Space: 45 MB (6.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -13,22 +13,8 @@ class Solution {
         List<Integer> list = set.stream()
             .toList();
         
-        System.out.println(list.size());
-        
-        // nums = new int[list.size()];
-        
-        for(int i=0;i<nums.length;i++) {
-            if (i<list.size()) {
+        for(int i=0;i<list.size();i++) {
                 nums[i]=list.get(i);    
-            }
-            else {
-                nums[i]=0;
-            }
-            
-        }
-        
-        for(int i : nums) {
-            System.out.print(" / " + i);
         }
         return list.size();
     }
